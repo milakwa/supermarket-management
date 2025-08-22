ab6bcd0b8b142ffb3d7ffb10062ed87a42f67935
milakwa <malakalamir55@gmail.com>
2025-08-23 02:00:27 +0300

    Added all project files

--- supermarket management/Program.cs
+++ supermarket management/Program.cs
@@ -1,10 +1,167 @@
-﻿namespace supermarket_management
+﻿namespace supermarket_management 
 {
+    public class Product
+    {
+        // Product attributes 
+        public string Name;
+        public string Category;
+        public int Quantity;
+        public DateTime ProductionDate;
+        public DateTime ExpiryDate;
+        // Constructor: it calls when we add a new Product and fills in attributes
+        public Product(string name, string category, int quantity, DateTime productionDate, DateTime expiryDate)
+        {
+            Name = name;
+            Category = category;
+            Quantity = quantity;
+            ProductionDate = productionDate;
+            ExpiryDate = expiryDate;
+        }
+        public void Display()
+        {
+            Console.WriteLine(Name + ", " + Category + ", " + Quantity + ", " + ProductionDate + ", " + ExpiryDate);
+        }
+    }
+    public static class DataStore
+    {
+        public static List<Product> products = new List<Product>();
+        public static string filePath = "Products.txt";
+
+        public static void loadProducts()
+        {
+            if (File.Exists(filePath))
+            {
+                foreach (string line in File.ReadAllLines(filePath))
+                {
+                    string[] parts = line.Split(',');  // To line Split from file 
+                    Product p = new Product(
+                        parts[0],                      // Name
+                        parts[1],                      // Category
+                        int.Parse(parts[2]),           // Quantity
+                        DateTime.Parse(parts[3]),      // ProductionDate
+                        DateTime.Parse(parts[4])       // ExpiryDate
+                        );
+                    products.Add(p);
+                }
+            }
+
+
+        }
+        public static void saveProducts()
+        {
+            List<string> lines = new List<string>();
+            foreach (Product p in products)
+            {
+                lines.Add($"{p.Name} ,{p.Category} ,{p.Quantity} ,{p.ProductionDate} ,{p.ExpiryDate}.");
+            }
+
+            File.WriteAllLines(filePath, lines);
+        }
+
+
+    }
+    public class Admin
+    {
+        private string Username;
+        private string Password;
+        public string username { get; set; }
+        public string password { get; set; }
+
+        public Admin(string user, string pass)
+        {
+            username = user;
+            password = pass;
+        }
+        public void AddProduct(Product p, int q)
+        {
+
+
+            DataStore.products.Add(p);
+
+        }
+        public void viewExpiryAlerts()
+        {
+
+            DateTime today;
+            bool valid = false;
+
+
+            while (!valid)
+            {
+                Console.Write("Enter today's date (yyyy-MM-dd): ");
+                string input = Console.ReadLine();
+
+                if (DateTime.TryParse(input, out today))
+                {
+                    valid = true;
+
+                    bool found = false;
+
+                    foreach (Product p in DataStore.products)
+                    {
+                        TimeSpan diff = p.expiryDate - today;
+
+                        if (diff.TotalDays <= 7 && diff.TotalDays >= 0)
+                        {
+                            Console.WriteLine($"{p.name} (Qty: {p.quantity}) is near expiry.");
+                            found = true;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        Console.WriteLine("No products near expiry.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid date format! Please try again.");
+                }
+            }
+        }
+        public bool checkExpiry(Product p)
+        {
+            DateTime today = DateTime.Now;
+            TimeSpan diff = p.ExpiryDate - today;
+
+            if (diff.TotalDays <= 7 && diff.TotalDays >= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Admin a = new Admin("malak", "1234");
+            int n = 2;
+            while (n>0)
+            {
+                
+                string name;
+                
+                string c;
+
+                int q;
+                Console.Write("productionDate:");
+                DateTime p =DateTime.Parse(Console.ReadLine());
+                Console.Write("expirydate:");
+                DateTime e = DateTime.Parse(Console.ReadLine());
+                Product product = new Product(name="malak", c="hhh", q=90, p, e);
+                
+                a.AddProduct(product, product.Quantity);
+                n--;
+            }
+            
+
+            a.viewExpiryAlerts();
+
+
         }
     }
 }
---
+++ supermarket management/sohila.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supermarket_management
+{
+    class P
+    {
+        static void Main(string[] args)
+        {
+            displayCart();
+        }
+        static void displayCart()
+        {
+            string[] itemsBought = new string[] {" Bread  ", " Eggs   ", " Chips  ", " Pasta  ", " Soda   ", " cereal ", " Bananas", " Butter ",
+            " Sugar  ", "Milk   ", "Yogurt "};
+            Console.WriteLine("\n\n\t\t\t\t    --* Cart *--");
+            Console.WriteLine("\n\t\t\t |  No.  | |    Items purchased    |\n");
+
+            for (int i = 0; i < itemsBought.Length; i++)
+            {
+                Console.WriteLine($"\t\t\t |   {i + 1}   | |        {itemsBought[i]}       |\n");
+            }
+        }
+
+    }
+    public class Customer
+    {
+        public string Name { get; set; }
+        public List<Product> cart { get; set; }
+
+        public Customer(string name)
+        {
+            Name = name;
+            cart = new List<Product>();
+        }
+        public void buyProduct(Product p, int quantity)
+        {
+            for (int i = 0; i < quantity; i++)
+            {
+                cart.Add(p);
+            }
+        }
+    }
+    /* public void AddProduct1(Product p, int q)
+     {
+
+
+         string path = @"C:\Users\sohil\Documents\Products.txt";
+         using (StreamWriter sw = new StreamWriter(path, true))
+         {
+             sw.WriteLine($"{p.name},{p.category},{p.quantity},{p.productionDate},{p.expiryDate}");
+         }
+
+         Console.WriteLine("Product Added");
+     }
+     public void ViewProducts()
+     {
+         string path = @"C:\Users\sohil\Documents\Products.txt";
+         string[] alllines = File.ReadAllLines(path);
+         Console.WriteLine("Products List :");
+         foreach (string line in alllines)
+         {
+             Console.WriteLine(line);
+         }
+     }*/
+
+
+}
+
